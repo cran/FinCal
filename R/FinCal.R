e1cf0dab1d04da2f1d309501c63f233d166a70a7
Felix Yanhui Fan <nolanfyh@gmail.com>
2013-08-13 12:11:37 +0000

    version 0.3

--- R/FinCal.R
+++ R/FinCal.R
@@ -337,6 +337,7 @@ hpr2ear <- function(hpr,t){
 #'
 #' @param r stated annual rate
 #' @seealso \code{\link{ear}}
+#' @seealso \code{\link{r.norminal}}
 #' @export
 #' @examples
 #' ear.continuous(r=0.1)
@@ -362,6 +363,7 @@ r.continuous <- function(r,m) {
 #' @param rc continuous compounded rate
 #' @param m number of desired times compounded each year
 #' @seealso \code{\link{r.continuous}}
+#' @seealso \code{\link{ear.continuous}}
 #' @export
 #' @examples
 #' r.norminal(0.03,1)
@@ -498,5 +500,469 @@ ear2bey <- function(ear){
   return(((1 + ear)^0.5 - 1)*2)
 }
 
+#' Weighted mean as a portfolio return
+#' 
+#' @param r returns of the individual assets in the portfolio
+#' @param w corresponding weights associated with each of the individual assets
+#' @export
+#' @examples
+#' wpr(r=c(0.12, 0.07, 0.03),w=c(0.5,0.4,0.1))
+wpr <- function(r,w){
+  if(sum(w) != 1){
+    warning("sum of weights is NOT equal to 1!")
+    return(sum(r*w))
+  }else{
+    return(sum(r*w))
+  }
+}
 
+#' Geometric mean return
+#' 
+#' @param r returns over multiple periods
+#' @export
+#' @examples
+#' geometric.mean(r=c(-0.0934, 0.2345, 0.0892))
+geometric.mean <- function(r){
+  rs <- r + 1
+  return(prod(rs)^(1/length(rs))-1)
+}
+
+#' harmonic mean, average price
+#' @param p price over multiple periods
+#' @export
+#' @examples
+#' harmonic.mean(p=c(8,9,10))
+harmonic.mean <- function(p){
+  return(1/mean(1/p))
+}
+
+#' Computing Coefficient of variation
+#'
+#' @param sd standard deviation
+#' @param avg average value
+#' @seealso \code{\link{Sharpe.ratio}}
+#' @export
+#' @examples
+#' coefficient.variation(sd=0.15,avg=0.39)
+coefficient.variation <- function(sd, avg){
+  return(sd/avg)
+}
+
+#' Computing Sharpe Ratio
+#'
+#' @param rp portfolio return
+#' @param rf risk-free return
+#' @param sd standard deviation of portfolio retwns
+#' @seealso \code{\link{coefficient.variation}}
+#' @seealso \code{\link{SFRatio}}
+#' @export
+#' @examples
+#' Sharpe.ratio(rp=0.038,rf=0.015,sd=0.07)
+Sharpe.ratio <- function(rp,rf,sd){
+  return((rp-rf)/sd)
+}
+
+#' Computing Roy's safety-first ratio
+#' 
+#' @param rp portfolio return
+#' @param rl threshold level return
+#' @param sd standard deviation of portfolio retwns
+#' @seealso \code{\link{Sharpe.ratio}}
+#' @export
+#' @examples
+#' SFRatio(rp=0.09,rl=0.03,sd=0.12)
+SFRatio <- function(rp, rl ,sd){
+  return((rp-rl)/sd)
+}
+
+#' Computing Sampling error
+#' 
+#' @param sm sample mean
+#' @param mu population mean
+#' @export
+#' @examples
+#' sampling.error(sm=0.45, mu=0.5)
+sampling.error <- function(sm,mu){
+  return(sm-mu)
+}
+
+#' Download stock prices from Yahoo Finance (open, high, low, close, volume, adjusted)
+#' 
+#' @param symbol symbol of stock, e.g. AAPL, GOOG, SPX
+#' @param start start date, e.g., 2013-07-31
+#' @param end end date, e.g., 2013-08-06
+#' @param freq time interval, e.g., d:daily, w:weekly, m:monthly
+#' @seealso \code{\link{get.ohlcs.yahoo}}
+#' @seealso \code{\link{get.ohlc.google}}
+#' @export
+#' @examples
+#' get.ohlc.yahoo(symbol="AAPL")
+#' @examples
+#' get.ohlc.yahoo(symbol="AAPL",start="2013-08-01",freq="d")
+#' @examples
+#' get.ohlc.yahoo(symbol="AAPL",start="2013-07-01",end="2013-08-01",freq="w")
+get.ohlc.yahoo <- function(symbol,start="firstDay",end="today",freq="d"){
+  if(start == "firstDay"){
+      URL=paste("http://ichart.finance.yahoo.com/table.csv?s=", symbol, sep="")
+      dat <- read.csv(URL)
+      dat=dat[order(dat$Date),]
+  }else{
+    temp <- strsplit(start,"-")
+    a=sprintf('%.2d',as.integer(temp[[1]][2])-1)
+    b=temp[[1]][3]
+    c=temp[[1]][1]
+    if(end != "today"){
+      temp <- strsplit(end,"-")
+      d=sprintf('%.2d',as.integer(temp[[1]][2])-1)
+      e=temp[[1]][3]
+      f=temp[[1]][1]
+    }else{
+      end=as.character(Sys.Date())
+      temp <- strsplit(end,"-")
+      d=sprintf('%.2d',as.integer(temp[[1]][2])-1)
+      e=temp[[1]][3]
+      f=temp[[1]][1]
+    }
+        URL=paste("http://ichart.finance.yahoo.com/table.csv?s=",symbol,"&a=",a,"&b=",b,"&c=",c,"&d=",d,"&e=",e,"&f=",f,"&g=",freq, sep="")
+        dat <- read.csv(URL)
+        dat=dat[order(dat$Date),]
+  } 
+colnames(dat) <- c("date", "open", "high", "low", "close", "volume", "adjusted")
+return(dat)
+}
+
+#' Download stock prices from Google Finance (open, high, low, close, volume)
+#' 
+#' @param symbol symbol of stock, e.g. AAPL, GOOG, SPX
+#' @param start start date, e.g., 2013-07-31
+#' @param end end date, e.g., 2013-08-06
+#' @seealso \code{\link{get.ohlc.yahoo}}
+#' @seealso \code{\link{get.ohlcs.google}}
+#' @export
+#' @examples
+#' get.ohlc.google(symbol="AAPL")
+#' @examples
+#' get.ohlc.google(symbol="AAPL",start="2013-08-01")
+#' @examples
+#' get.ohlc.google(symbol="AAPL",start="2013-07-01",end="2013-08-01")
+get.ohlc.google <- function(symbol,start="2013-01-01",end="today"){
+  require(RCurl)
+    temp <- strsplit(start,"-")
+    a=month.abb[as.numeric(temp[[1]][2])]
+    b=temp[[1]][3]
+    c=temp[[1]][1]
+    if(end != "today"){
+      temp <- strsplit(end,"-")
+      d=month.abb[as.numeric(temp[[1]][2])]
+      e=temp[[1]][3]
+      f=temp[[1]][1]
+    }else{
+      end=as.character(Sys.Date())
+      temp <- strsplit(end,"-")
+      d=month.abb[as.numeric(temp[[1]][2])]
+      e=temp[[1]][3]
+      f=temp[[1]][1]
+    }
+    URL=paste("https://www.google.com/finance/historical?q=",symbol,"&output=csv","&startdate=",a,"+",b,"+",c,"&enddate=",d,"+",e,"+",f, sep="")
+    myCsv <- getURL(URL, ssl.verifypeer = FALSE)
+    dat <- read.csv(textConnection(myCsv))
+    colnames(dat) <- c("date", "open", "high", "low", "close", "volume")
+    dates=as.character(dat$date)
+    for(i in 1:length(dates)){
+      tempdates=strsplit(dates[i],"-")
+      tempdates[[1]][2]=match(tempdates[[1]][2],month.abb)
+      tempd=paste(tempdates[[1]][1],tempdates[[1]][2],tempdates[[1]][3],sep="-")
+      dates[i]=format(as.Date(tempd,"%d-%m-%y"),"%Y-%m-%d")
+    }
+    dat$date=dates
+    dat=dat[order(dat$date),]
+    return(dat)
+}
+
+#' Batch download stock prices from Yahoo Finance (open, high, low, close, volume, adjusted)
+#' 
+#' @param symbols symbols of stock, e.g. AAPL, GOOG, SPX
+#' @param start start date, e.g., 2013-07-31
+#' @param end end date, e.g., 2013-08-06
+#' @param freq time interval, e.g., d:daily, w:weekly, m:monthly
+#' @seealso \code{\link{get.ohlc.yahoo}}
+#' @seealso \code{\link{get.ohlcs.google}}
+#' @export
+#' @examples
+#' get.ohlcs.yahoo(symbols=c("AAPL","GOOG","SPY"),freq="d")
+#' @examples
+#' get.ohlcs.yahoo(symbols=c("AAPL","GOOG","SPY"),start="2013-01-01",freq="m")
+get.ohlcs.yahoo <- function(symbols,start="firstDay",end="today",freq="d"){
+  n <- length(symbols)
+  ohlc=list()
+  if(start == "firstDay"){
+      for(i in 1:n){
+        URL=paste("http://ichart.finance.yahoo.com/table.csv?s=", symbols[i], sep="")
+        dat <- read.csv(URL)
+        dat=dat[order(dat$Date),]
+        colnames(dat) <- c("date", "open", "high", "low", "close", "volume", "adjusted")
+        ohlc[[symbols[i]]]=dat
+      }
+  }else{
+    temp <- strsplit(start,"-")
+    a=sprintf('%.2d',as.integer(temp[[1]][2])-1)
+    b=temp[[1]][3]
+    c=temp[[1]][1]
+    if(end != "today"){
+      temp <- strsplit(end,"-")
+      d=sprintf('%.2d',as.integer(temp[[1]][2])-1)
+      e=temp[[1]][3]
+      f=temp[[1]][1]
+    }else{
+      end=as.character(Sys.Date())
+      temp <- strsplit(end,"-")
+      d=sprintf('%.2d',as.integer(temp[[1]][2])-1)
+      e=temp[[1]][3]
+      f=temp[[1]][1]
+    }
+    
+    for(i in 1:n){
+      URL=paste("http://ichart.finance.yahoo.com/table.csv?s=",symbols[i],"&a=",a,"&b=",b,"&c=",c,"&d=",d,"&e=",e,"&f=",f,"&g=",freq, sep="")
+      dat <- read.csv(URL)
+      dat=dat[order(dat$Date),]
+      colnames(dat) <- c("date", "open", "high", "low", "close", "volume", "adjusted")
+      ohlc[[symbols[i]]]=dat
+    }
+  } 
+  return(ohlc)
+}
+
+#' Batch download stock prices from Google Finance (open, high, low, close, volume)
+#' 
+#' @param symbols symbols of stock, e.g. AAPL, GOOG, SPX
+#' @param start start date, e.g., 2013-07-31
+#' @param end end date, e.g., 2013-08-06
+#' @seealso \code{\link{get.ohlc.google}}
+#' @seealso \code{\link{get.ohlcs.yahoo}}
+#' @export
+#' @examples
+#' get.ohlcs.google(symbols=c("AAPL","GOOG","SPY"))
+#' @examples
+#' get.ohlcs.google(symbols=c("AAPL","GOOG","SPY"),start="2013-01-01")
+#' @examples
+#' get.ohlcs.google(symbols=c("AAPL","GOOG","SPY"),start="2013-01-01",end="2013-07-31")
+get.ohlcs.google <- function(symbols,start="2013-01-01",end="today"){
+  require(RCurl)
+  n <- length(symbols)
+  ohlc=list()
+  temp <- strsplit(start,"-")
+  a=month.abb[as.numeric(temp[[1]][2])]
+  b=temp[[1]][3]
+  c=temp[[1]][1]
+  if(end != "today"){
+    temp <- strsplit(end,"-")
+    d=month.abb[as.numeric(temp[[1]][2])]
+    e=temp[[1]][3]
+    f=temp[[1]][1]
+  }else{
+    end=as.character(Sys.Date())
+    temp <- strsplit(end,"-")
+    d=month.abb[as.numeric(temp[[1]][2])]
+    e=temp[[1]][3]
+    f=temp[[1]][1]
+  }
+  for(i in 1:n){
+    URL=paste("https://www.google.com/finance/historical?q=",symbols[i],"&output=csv","&startdate=",a,"+",b,"+",c,"&enddate=",d,"+",e,"+",f, sep="")
+    myCsv <- getURL(URL, ssl.verifypeer = FALSE)
+    dat <- read.csv(textConnection(myCsv))
+    colnames(dat) <- c("date", "open", "high", "low", "close", "volume")
+    dates=as.character(dat$date)
+    for(j in 1:length(dates)){
+      tempdates=strsplit(dates[j],"-")
+      tempdates[[1]][2]=match(tempdates[[1]][2],month.abb)
+      tempd=paste(tempdates[[1]][1],tempdates[[1]][2],tempdates[[1]][3],sep="-")
+      dates[j]=format(as.Date(tempd,"%d-%m-%y"),"%Y-%m-%d")
+    }
+    dat$date=dates
+    dat=dat[order(dat$date),]
+    ohlc[[symbols[i]]]=dat
+  }
+  return(ohlc)
+}
+
+#' Technical analysts - Line charts: show prices for each period as a continuous line
+#' 
+#' @param ohlc output from get.ohlc.yahoo or get.ohlc.google
+#' @param y y coordinates: close, open, high, low or adjusted
+#' @param main an overall title for the plot
+#' @param ... Arguments to be passed to methods
+#' @seealso \code{\link{get.ohlc.yahoo}}
+#' @seealso \code{\link{get.ohlc.google}}
+#' @export
+#' @examples
+#' google <- get.ohlc.yahoo("GOOG"); lineChart(google)
+#' @examples
+#' apple <- get.ohlc.google("AAPL"); lineChart(apple)
+lineChart <- function(ohlc,y="close",main="",...){
+  options(warn=-1)
+  require(ggplot2)
+  require(scales)
+  ohlc$date <- as.Date(ohlc$date,"%Y-%m-%d")
+  myBreak=date.breaks(ohlc)
+  if(y == "close"){
+    ggplot(ohlc, aes_string(x='date',y='close')) + geom_line() + labs( title =main ) + labs(x="") + 
+      theme(axis.text.x=element_text(angle=90)) + 
+      scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+  }else if(y == "open"){
+    ggplot(ohlc, aes_string(x='date',y='open')) + geom_line() + labs( title =main ) + labs(x="") + 
+      theme(axis.text.x=element_text(angle=90)) + 
+      scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+  }else if(y == "high"){
+    ggplot(ohlc, aes_string(x='date',y='high')) + geom_line() + labs( title =main ) + labs(x="") + 
+      theme(axis.text.x=element_text(angle=90)) + 
+      scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+  }else if(y == "low"){
+    ggplot(ohlc, aes_string(x='date','y=low')) + geom_line() + labs( title =main ) + labs(x="") + 
+      theme(axis.text.x=element_text(angle=90)) + 
+      scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+  }else if(y == "adjusted"){
+    ggplot(ohlc, aes_string(x='date',y='adjusted')) + geom_line() + labs( title =main ) + labs(x="") + 
+      theme(axis.text.x=element_text(angle=90)) + 
+      scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+  }
+}
+
+#' Technical analysts - Candlestick chart: show prices for each period as a continuous line. The box is clear if the closing price is higher than the opening price, or filled red if the closing is lower than the opening price.
+#' 
+#' @param ohlc output from get.ohlc.yahoo or get.ohlc.google
+#' @param start start date to plot, if not specified, all date in ohlc will be included
+#' @param end end date to plot
+#' @param main an overall title for the plot
+#' @param ... Arguments to be passed to methods
+#' @seealso \code{\link{get.ohlc.yahoo}}
+#' @seealso \code{\link{get.ohlc.google}}
+#' @export
+#' @examples
+#' google <- get.ohlc.yahoo("GOOG",start="2013-07-01",end="2013-08-01"); candlestickChart(google)
+#' @examples
+#' apple <- get.ohlc.google("AAPL",start="2013-07-01",end="2013-08-01"); candlestickChart(apple)
+candlestickChart <- function(ohlc, start=NULL, end=NULL, main="", ...){
+  options(warn=-1)
+  require(ggplot2)
+  require(scales)
+  date <- as.Date(ohlc$date)
+  open <- as.vector(ohlc$open)
+  high <- as.vector(ohlc$high)
+  low <- as.vector(ohlc$low)
+  close <- as.vector(ohlc$close)
+  
+  xSubset <-data.frame('date'=date,'open'=open,'high'= high,'low'=low,'close'=close)
+   
+  xSubset$candleLower <- pmin(xSubset$open, xSubset$close)
+  xSubset$candleMiddle <- NA
+  xSubset$candleUpper <- pmax(xSubset$open, xSubset$close)
+  xSubset$fill <- 'red'
+  xSubset$fill[xSubset$open < xSubset$close] = ''
+  
+  if(!is.null(start) & !is.null(end)){
+    start=as.Date(start)
+    end=as.Date(end)
+    xSubset <-subset(xSubset, xSubset$date > start & xSubset$date < end) 
+  }
+  myBreak=date.breaks(xSubset)
+  g <- ggplot(xSubset, aes_string(x='date', lower='candleLower', middle='candleMiddle', upper='candleUpper', ymin='low', ymax='high',na.rm=TRUE)) +
+    geom_boxplot(stat='identity', aes_string(group='date', fill='fill')) + theme_bw() + 
+    scale_fill_manual(name = "", values = c("white", "red")) + labs( title =main ) + labs(x="") +
+    theme(legend.position="none") + theme(axis.text.x=element_text(angle=90)) +
+    scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+  return(g)
+}
+
+#' Technical analysts - Volume charts: show each period's volume as a vertical line
+#' 
+#' @param ohlc output from get.ohlc.yahoo or get.ohlc.google
+#' @param main an overall title for the plot
+#' @param ... Arguments to be passed to methods
+#' @seealso \code{\link{get.ohlc.yahoo}}
+#' @seealso \code{\link{get.ohlc.google}}
+#' @export
+#' @examples
+#' google <- get.ohlc.yahoo("GOOG"); volumeChart(google)
+#' @examples
+#' apple <- get.ohlc.google("AAPL"); volumeChart(apple)
+volumeChart <- function(ohlc,main="",...){
+  options(warn=-1)
+  require(ggplot2)
+  require(scales)
+  ohlc$date <- as.Date(ohlc$date,"%Y-%m-%d")
+  myBreak=date.breaks(ohlc)
+  g=ggplot(ohlc, aes_string(x='date',y='volume')) + geom_bar(stat="identity") + labs( title =main ) + labs(x="") + 
+    theme(axis.text.x=element_text(angle=90)) + 
+    scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+  return(g)
+}
+
+#' Deciding width (days or weeks) for date_breaks in plotting
+#' 
+#' @param ohlc output from get.ohlc.yahoo or get.ohlc.google
+#' @export
+#' @examples
+#' google <- get.ohlc.yahoo("GOOG"); date.breaks(google)
+date.breaks <- function(ohlc){
+  n=length(ohlc$date)
+  myBreak="1 day"
+  if(n<30){
+    myBreak="1 day"
+  }else if(n < 90){
+    myBreak="3 days"
+  }else if(n < 140){
+    myBreak="1 week"
+  }else{
+    m=n/140
+    myBreak=paste(m, "weeks", sep=" ")
+  }
+  return(myBreak)
+}
+
+#' Technical analysts - Line charts: show prices for each period as a continuous line for multiple stocks
+#' 
+#' @param ohlcs output from get.ohlc.yahoo.mult or get.ohlc.google.mult
+#' @param y y coordinates: Close, Open, High, Low or Adj.Close
+#' @param main an overall title for the plot
+#' @param ... Arguments to be passed to methods
+#' @seealso \code{\link{get.ohlcs.yahoo}}
+#' @seealso \code{\link{get.ohlcs.google}}
+#' @seealso \code{\link{lineChart}}
+#' @export
+#' @examples
+#' googapple <- get.ohlcs.yahoo(c("GOOG","AAPL"),start="2013-01-01"); lineChartMult(googapple)
+#' @examples
+#' googapple <- get.ohlcs.google(c("GOOG","AAPL"),start="2013-01-01"); lineChartMult(googapple)
+lineChartMult <- function(ohlcs,y="close",main="",...){
+  options(warn=-1)
+  require(ggplot2)
+  require(scales)
+  require("reshape2")
+  sname=names(ohlcs)
+  n=length(sname)
+  Date=ohlcs[[sname[1]]]$date <- as.Date(ohlcs[[sname[1]]]$date,"%Y-%m-%d")
+  myBreak=date.breaks(ohlcs[[sname[1]]])
+  z=data.frame(Date)
+  colname=c("Date")
+  for(i in 1:n){
+    colname=append(colname,sname[i])
+    if(y == "close"){
+      z[,i+1] = ohlcs[[sname[i]]]$close
+    }else if(y == "ppen"){
+      z[,i+1] = ohlcs[[sname[i]]]$open
+    }else if(y == "high"){
+      z[,i+1] = ohlcs[[sname[i]]]$high
+    }else if(y == "low"){
+      z[,i+1] = ohlcs[[sname[i]]]$low
+    }else if(y == "adjusted"){
+      z[,i+1] = ohlcs[[sname[i]]]$adjusted
+    }
+  }
+  colnames(z) <- colname
+  pdf <- melt(z, id="Date")
+  ggplot(data=pdf, aes_string(x='Date', y='value', colour='variable')) + geom_line() +
+    labs( title =main ) + labs(x="") + scale_y_continuous(name=y) +
+    theme(axis.text.x=element_text(angle=90)) + theme(legend.title=element_blank()) +
+    scale_x_date(labels = date_format("%Y-%m-%d"),breaks=date_breaks(myBreak))
+}
 
